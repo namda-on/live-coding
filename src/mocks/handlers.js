0bd98190fdeb452d1f2740bd70eb19d74c81d777
Jinyong Park <jinyoung1997@gmail.com>
2024-05-07 18:30:18 +0900

    feat : create handler 추가

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,12 +1,34 @@
 import { http, HttpResponse } from "msw";
 import { mockTree } from "./mockData";
 
+let idx = 9;
+
 export const handlers = [
   http.get("/tree", () => {
     return HttpResponse.json(mockTree);
   }),
 
-  http.post("/tree", ({ request }) => {
+  http.post("/tree", async ({ request }) => {
+    const { prevId, parentId } = await request.json();
+
+    // Create a new node
+    const targetParent = parentId === null ? mockTree : mockTree.find((node) => node.id === parentId);
+    const newNode = {
+      id: idx,
+      name: "Untitled",
+      type: "FILE",
+      parentId: parentId,
+      nextNodeId: null,
+      prevNodeId: prevId,
+    };
+
+    if (Array.isArray(targetParent)) {
+      targetParent.push(newNode);
+    } else {
+      targetParent.children.push(newNode);
+    }
+    idx++;
+
     return HttpResponse.json({ name: "John" });
   }),
 ];
