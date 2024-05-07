acc3cb3f3301f18e0f5671805825031a49cf06eb
Jinyong Park <jinyoung1997@gmail.com>
2024-05-07 17:37:27 +0900

    feat : node select 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
+import { useState, createContext, useMemo } from "react";
 import useTree from "./hooks/useTree";
 import FileTree from "./FileTree";
 import "./app.css";
 
+export const SelectedTreeContext = createContext(null);
+
 function App() {
   const { tree: result, isLoading } = useTree();
+  const [selectedTreeNode, setSelectedTreeNode] = useState(null);
+
+  const contextValue = useMemo(
+    () => ({ selectedTreeNode, setSelectedTreeNode }),
+    [selectedTreeNode, setSelectedTreeNode]
+  );
 
   if (isLoading) return <div>Loading </div>;
 
   return (
-    <div>
-      <button className="create_button">create</button>
-      <FileTree tree={result} />
-    </div>
+    <SelectedTreeContext.Provider value={contextValue}>
+      <div>
+        <button className="create_button">create</button>
+        <FileTree tree={result} setSelectedTreeNode={setSelectedTreeNode} />
+      </div>
+    </SelectedTreeContext.Provider>
   );
 }
 
--- src/FileTreeNode.jsx
+++ src/FileTreeNode.jsx
@@ -1,7 +1,21 @@
+import { useContext, useMemo } from "react";
+import { SelectedTreeContext } from "./App";
+
 function FileTreeNode({ node, children }) {
+  const { selectedTreeNode, setSelectedTreeNode } = useContext(SelectedTreeContext);
+
+  const handleClick = () => {
+    setSelectedTreeNode(node);
+  };
+
+  const isSelected = useMemo(() => node.id === selectedTreeNode?.id, [selectedTreeNode]);
+  const selectedStyle = useMemo(() => (isSelected ? { backgroundColor: "gray" } : {}), [isSelected]);
+
   return (
     <li className="tree_node">
-      <div className="tree_node_content">{node.name}</div>
+      <div onClick={handleClick} className="tree_node_content" style={selectedStyle}>
+        {node.name}
+      </div>
       {children}
     </li>
   );
